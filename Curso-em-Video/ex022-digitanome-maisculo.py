ff70231ba3199eadbc578a409099ca29cd85739d
Willian Teixeira <112757886+willianteixeiradias@users.noreply.github.com>
2022-09-17 14:00:35 +0000

    Create ex022-digitanome-maisculo.py

---
+++ Curso-em-Video/ex022-digitanome-maisculo.py
@@ -0,0 +1,8 @@
+nome = str (input('digite seu nome completo').strip())
+#nome = nome.upper()
+print('seu nome completo em maiusculo é: {}'.format(( nome.upper)()))
+print('seu nome completo em minusculo é: {}'.format(( nome.lower)()))
+print (' o total de letras do seu nome é:{}'.format( len (nome) - nome.count(' ')))
+print (seu primennome.find(' ')
+
+#print('seu nome tem este total de letras : {}ana '.format(( nome.len(nome)- nome.count(' '))()))
