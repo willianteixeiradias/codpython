c953dde47528562aad76e0a94abd8d42263c417f
Willian Teixeira <112757886+willianteixeiradias@users.noreply.github.com>
2022-09-24 13:27:22 +0000

    Create ex24-analisar-nome.py

---
+++ Curso-em-Video/ex24-analisar-nome.py
@@ -0,0 +1,7 @@
+nome = str(input('digite seu nome')).strip()
+print('seu nome tem silva?{}'.format('silva' in nome.lower()))
+
+#ex 24 nomecidade = str(input('digite o nome da cidade:')).strip()
+#print (nomecidade[:5].upper() == 'SANTO')
+
+
