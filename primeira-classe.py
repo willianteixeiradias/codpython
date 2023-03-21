7d448ff558454610fd39ee070ff6c35ea358d56e
Willian Teixeira <112757886+willianteixeiradias@users.noreply.github.com>
2023-03-21 09:19:13 +0000

    Create primeira-classe.py

---
+++ primeira-classe.py
@@ -0,0 +1,7 @@
+class NomeWillian: #criando a classe
+    def imprimir_nome(self,nome): #definindo o metodo
+        print(f"ola {nome},voce criou a sua primeira classe")
+        #chamando a classe
+
+objeto1 = NomeWillian() #estancioando um objeto do tipo nomewillian
+objeto1.imprimir_nome('teixeira') #invocando o metodo
